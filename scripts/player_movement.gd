feddb9d9b2d8a13b12a6705d266196a79371a8ad
Dylan Tseng <108163886+Deeeeelan@users.noreply.github.com>
2026-07-07 11:33:57 -0700

    Add slam

--- scripts/player_movement.gd
+++ scripts/player_movement.gd
@@ -5,6 +5,7 @@ const BASE_SPEED: float = 800.0
 const MAX_SPEED: float = 1300.0
 var current_speed: float = 0.0
 var curr_accel: float = 0.0
+const ACCELERATION: float = 0.2
 
 const BASE_JUMP: float = -600.0
 const LERP_SPEED: float = 0.08
@@ -13,8 +14,9 @@ const MAX_JUMPS: int = 2
 
 var dashes: int = 0
 const MAX_DASHES: int = 1
-const DASH_VEL = 3000
+const DASH_VEL: float  = 3000
 var dash_cd = false
+const DASH_TIME: float  = 0.75
 
 
 var direction: float = 0
@@ -27,6 +29,8 @@ var coyote_time_valid = false
 var coyote_time_started = false
 const COYOTE_TIME = 0.22
 
+const SLAM_GRAV_MULT: float = 4.0
+var can_slam = false
 func process_jump_y(j_add: bool):
 	if j_add: jumps += 1
 	
@@ -46,7 +50,8 @@ func _physics_process(delta: float) -> void:
 	# Add the gravity.
 	if not is_on_floor(): #TODO: add slam particles
 		if Input.is_action_pressed("down"):
-			velocity += (get_gravity() * 3) * delta
+			can_slam = true
+			velocity += (get_gravity() * SLAM_GRAV_MULT) * delta
 		else:
 			if is_on_wall():
 				velocity += (get_gravity() / 2) * delta
@@ -65,6 +70,10 @@ func _physics_process(delta: float) -> void:
 			#elif not coyote_time_valid:
 				#jumps = 1
 	else:
+		if Input.is_action_pressed("down") and can_slam:
+			print("SLAM (placeholder)")
+		can_slam = false
+			
 		coyote_time_started = false
 		coyote_time_valid = false
 		jumps = 0
@@ -82,15 +91,12 @@ func _physics_process(delta: float) -> void:
 		if get_jump_condition(false):
 			process_jump_y(true)
 	
-
-
-	
 	direction = Input.get_axis("left", "right")
 	if direction:
 		current_speed = lerpf(BASE_SPEED, MAX_SPEED, curr_accel)
 		velocity.x = lerp(velocity.x, direction * current_speed, LERP_SPEED)
 		if last_dir == direction and not is_on_wall() and curr_accel < 1.0:
-			curr_accel = minf(curr_accel + (0.2 * delta), 1.0)
+			curr_accel = minf(curr_accel + (ACCELERATION * delta), 1.0)
 		else:
 			last_dir = direction
 			curr_accel = 0.2
@@ -101,7 +107,7 @@ func _physics_process(delta: float) -> void:
 	if Input.is_action_just_pressed("dash") and dashes < MAX_DASHES and not dash_cd:
 		dash_cd = true
 		velocity.x = last_dir * DASH_VEL
-		get_tree().create_timer(0.75).timeout.connect(func():
+		get_tree().create_timer(DASH_TIME).timeout.connect(func():
 			dash_cd = false
 		)
 		
