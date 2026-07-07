ddd7f3e7941a13b4c4dc3947841871e027121d91
Dylan Tseng <108163886+Deeeeelan@users.noreply.github.com>
2026-07-07 10:52:32 -0700

    Add player acceleration and double jump

--- scripts/player_movement.gd
+++ scripts/player_movement.gd
@@ -1,26 +1,51 @@
 extends CharacterBody2D
 
 
-const SPEED = 600.0
-const JUMP_VELOCITY = -500.0
-const LERP_SPEED = 0.08
+const BASE_SPEED: float = 600.0
+const MAX_SPEED: float = 1000.0
+var current_speed: float = 0.0
+var curr_accel: float = 0.0
 
+const BASE_JUMP: float = -600.0
+const LERP_SPEED: float = 0.08
+var jumps: int = 0
+const MAX_JUMPS: int = 2
+
+var direction: float = 0
+var last_dir: float = 0
+var jmp_debounce = false
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
+		if jumps == 0:
+			jumps = 1
+	else:
+		jumps = 0
 
 	# Handle jump.
-	if Input.is_action_just_pressed("jump") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
+	if Input.is_action_just_pressed("jump") and jumps < MAX_JUMPS and not jmp_debounce:
+		jumps += 1
+		print(jumps)
+		velocity.y = BASE_JUMP
+		get_tree().create_timer(0.1).timeout.connect(func():
+			jmp_debounce = false
+			)
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("left", "right")
+	direction = Input.get_axis("left", "right")
 	if direction:
-		velocity.x = lerp(velocity.x, direction * SPEED, LERP_SPEED)
+		current_speed = lerpf(BASE_SPEED, MAX_SPEED, curr_accel)
+		velocity.x = lerp(velocity.x, direction * current_speed, LERP_SPEED)
+		if last_dir == direction:
+			curr_accel += 0.05 # TODO: DELTATIME
+		else:
+			last_dir = direction
+			curr_accel = 0.4
 	else:
-		velocity.x = lerp(velocity.x, move_toward(velocity.x, 0, SPEED), LERP_SPEED)
+		velocity.x = lerp(velocity.x, move_toward(velocity.x, 0, current_speed), LERP_SPEED)
+		curr_accel = 0
 
 	move_and_slide()
