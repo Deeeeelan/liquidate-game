e1be9bc65e40354416ce85be5bca90fc61941d73
Dylan Tseng <108163886+Deeeeelan@users.noreply.github.com>
2026-07-07 23:02:29 -0700

    Add glass breaking

--- scripts/player_movement.gd
+++ scripts/player_movement.gd
@@ -50,6 +50,8 @@ var slide_slam_stop = false
 
 const GRAVITY_MULT: float = 1.5
 
+var has_dash_mom = false
+
 func debug_point(pos: Vector2, color: Color = Color(1.0, 1.0, 1.0, 1.0)):
 	var node = $Sprite2D.duplicate()
 	node.scale = Vector2(0.2, 0.2)
@@ -58,7 +60,7 @@ func debug_point(pos: Vector2, color: Color = Color(1.0, 1.0, 1.0, 1.0)):
 	node.global_position = pos
 
 func process_jump_y(j_add: bool):
-	if j_add: jumps += 1	
+	if j_add: jumps += 1
 	velocity.y = min(BASE_JUMP, velocity.y + BASE_JUMP)
 	
 	get_tree().create_timer(0.1).timeout.connect(func():
@@ -125,7 +127,18 @@ func interact_collide(area: Area2D):
 func _ready() -> void:
 	$FindTick.timeout.connect(find_tick)
 	$Interact.area_entered.connect(interact_collide)
+	
+
 func _physics_process(delta: float) -> void:
+	if has_dash_mom:
+		var space_state = get_world_2d().direct_space_state
+		var horiz_query = PhysicsRayQueryParameters2D.create(global_position, global_position + Vector2(direction * 128, 0))
+		horiz_query.exclude = [self]
+		var horiz_result = space_state.intersect_ray(horiz_query)
+
+		if horiz_result:
+			if horiz_result.collider.is_in_group("glass"):
+				horiz_result.collider.queue_free()
 	
 	# scrapped cuz its annoying to debug
 	## ledge grabbing: (I need to plan this out since I dunno how to visualize this)
@@ -148,17 +161,17 @@ func _physics_process(delta: float) -> void:
 		#air_query.position = offset_pos + Vector2(0, -180)
 		#var air_result = space_state.intersect_point(air_query)
 		#
-		## get_tree().create_timer(randf(0.005, 0.06)).timeout.connect(func():)
-		##debug_point(origin, Color(0.0, 0.651, 0.604, 1.0))
-		##debug_point(offset_pos, Color(0.0, 0.651, 0.604, 1.0))
-		## debug_point(offset_pos + Vector2(0, -180), Color(1.0, 0.208, 0.604, 1.0))
-		##debug_point(offset_pos + Vector2(0, -132), Color(1.0, 0.208, 0.604, 1.0))
-		##debug_point(offset_pos + Vector2(0, 32), Color(1.0, 0.208, 0.604, 1.0))
+		# get_tree().create_timer(randf(0.005, 0.06)).timeout.connect(func():)
+		#debug_point(origin, Color(0.0, 0.651, 0.604, 1.0))
+		#debug_point(offset_pos, Color(0.0, 0.651, 0.604, 1.0))
+		# debug_point(offset_pos + Vector2(0, -180), Color(1.0, 0.208, 0.604, 1.0))
+		#debug_point(offset_pos + Vector2(0, -132), Color(1.0, 0.208, 0.604, 1.0))
+		#debug_point(offset_pos + Vector2(0, 32), Color(1.0, 0.208, 0.604, 1.0))
 		#if horiz_result and down_result and (not air_result) and (not grab_ledge):
 			#grab_ledge = true
 			#var target_pos = down_result.position - Vector2(0, 64)
-			##debug_point(target_pos )
-			##debug_point(horiz_result.position, Color(0.722, 0.526, 0.0, 1.0))
+			#debug_point(target_pos )
+			#debug_point(horiz_result.position, Color(0.722, 0.526, 0.0, 1.0))
 #
 			#var tween = get_tree().create_tween().set_trans(Tween.TRANS_LINEAR)
 			#tween.tween_property(self, "global_position", target_pos, 0.15)
@@ -248,7 +261,11 @@ func _physics_process(delta: float) -> void:
 		if is_on_floor() and Input.is_action_pressed("down") and not slide_cd:
 			slide_cd = true
 			slide_slam_stop = true
+			has_dash_mom = true
 			velocity.x = direction * SLIDE_VEL
+			get_tree().create_timer(0.35).timeout.connect(func():
+				has_dash_mom = false
+			)
 			get_tree().create_timer(Slide_TIME).timeout.connect(func():
 				slide_cd = false
 			)
@@ -259,6 +276,10 @@ func _physics_process(delta: float) -> void:
 	if Input.is_action_just_pressed("dash") and dashes < MAX_DASHES and not dash_cd:
 		dash_cd = true
 		velocity.x = last_dir * DASH_VEL
+		has_dash_mom = true
+		get_tree().create_timer(0.35).timeout.connect(func():
+			has_dash_mom = false
+		)
 		get_tree().create_timer(DASH_TIME).timeout.connect(func():
 			dash_cd = false
 		)
