103c18c1b0882ffc7f77c50d20c54cd8456d9c54
Dylan Tseng <108163886+Deeeeelan@users.noreply.github.com>
2026-07-08 18:42:20 -0700

    Add end screen

--- scripts/game_script.gd
+++ scripts/game_script.gd
@@ -1,9 +1,31 @@
 extends Node
 
+var minute_left = false
+var game_ending = false
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	$GameTimer.timeout.connect(func():
 		if GameManager.game_started:
-			GameManager.time_left -= 1
+			if GameManager.time_left <= 0 and not game_ending:
+				game_ending = true
+				var tween = get_tree().create_tween()
+				tween.tween_property($CanvasLayer/Control/ColorRect, "color", Color("000000ff"), 3.0)
+				tween.play()
+				await tween.finished
+				Engine.time_scale = 0.0
+				%Stats.visible = true
+				var score_text = "Money Liquidated:\n" + str(GameManager.score) + "/10000000"
+				score_text += "Congratulations!" if GameManager.score > 10000000 else "yeah... you might get fired\n\n(hint: take more valuable things!)"
+				%ScoreText.text = score_text
+			else:
+				GameManager.time_left -= 1
+			
+			if GameManager.time_left <= 60 and not minute_left:
+				minute_left = true
+				var tween = get_tree().create_tween()
+				tween.tween_property($CanvasLayer/Control/ColorRect, "color", Color("e9000017"), 3.0)
+				tween.play()
+
+				
 	)
